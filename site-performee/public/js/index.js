a2107579db56f11ddbc061e5d2e79b6f825e6360
Luigi Vicchietti <vcchtt.yluigi22@gmail.com>
2023-09-02 03:44:10 -0300

    Adicionando detalhes e criação da sessão de integrantes

--- site-performee/public/js/index.js
+++ site-performee/public/js/index.js
@@ -1,7 +1,19 @@
-const navbar = document.getElementById("Nav");
+const navbar = document.getElementById("Nav")
+    , target = document.querySelectorAll("[element-anime]");
+    
+    const rolagem = () => {
+        const windowTop = window.pageYOffset + (window.innerHeight * 0.9);
+        navbar.classList.toggle("ativa", scrollY > 0);
 
-function rolagem() {
-    navbar.classList.toggle("ativa", scrollY > 0);
+    target.forEach(e => {
+        const elementTop = e.getBoundingClientRect().top + window.pageYOffset;
+        if (windowTop > elementTop) {
+            e.classList.add("animate");
+        } else {
+            e.classList.remove("animate");
+        }
+    });
 }
 
+window.addEventListener('load', rolagem);
 window.addEventListener('scroll', rolagem);
