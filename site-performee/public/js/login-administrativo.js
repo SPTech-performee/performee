e08e4360c451c2c42ff44cdf2be94ef150f1e358
Luigi Vicchietti <vcchtt.yluigi22@gmail.com>
2023-09-22 22:38:01 -0300

    responsividade da outra parte do login

--- site-performee/public/js/login-administrativo.js
+++ site-performee/public/js/login-administrativo.js
@@ -3,13 +3,13 @@ const target = document.querySelectorAll("[element-anime]")
     , direita = document.getElementById("Direita")
     , btnChange = document.querySelectorAll(".change");
 
-    const changeLogin = () => {
-        esquerda.classList.toggle("showing");
-        direita.classList.toggle("hide")
-    }
-    
-    const rolagem = () => {
-        const windowTop = window.pageYOffset + (window.innerHeight * 0.9);
+const changeLogin = () => {
+    esquerda.classList.toggle("showing");
+    direita.classList.toggle("hide");
+}
+
+const rolagem = () => {
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.9);
 
     target.forEach(e => {
         const elementTop = e.getBoundingClientRect().top + window.pageYOffset;
@@ -19,6 +19,24 @@ const target = document.querySelectorAll("[element-anime]")
             e.classList.remove("animate");
         }
     });
+}
+
+    if (window.screen.width <= 700) {
+        const arrowRight = document.getElementById("SetaVolta");
+        arrowRight.setAttribute('src', './assets/icons/arrow_back.png');
+    }
+
+if (window.screen.width <= 500) {
+    const submitContainer = document.querySelector(".submit-container");
+    submitContainer.innerHTML = `
+    <div class="esqueci-senha">
+        <span>Esqueceu a senha?</span>
+        <a href="#">Clique aqui</a>
+    </div>
+    <button class="select-disable" id="BtnClient">
+        ENTRAR
+    </button>
+    `;
 }
 
 window.addEventListener('load', rolagem);
