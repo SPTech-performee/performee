62a7350698e946418cd7048220120151216a06af
Luigi Vicchietti <vcchtt.yluigi22@gmail.com>
2023-10-08 20:05:21 -0300

    arrumando detalhes das telas já criadas

--- site-performee/public/area-restrita/js/dash-geral.js
+++ site-performee/public/area-restrita/js/dash-geral.js
@@ -20,8 +20,31 @@ const btnExpandir = document.getElementById('Expand')
     , inputLog = document.getElementById('InputLog')
     , slcLog = document.getElementById('SlcLog');
 
-    const expandirNav = () => {
-        nav.classList.toggle('opened');
+let navOpen = false;
+const expandirNav = () => {
+    nav.classList.toggle('opened');
+    if (navOpen) {
+        navOpen = false;
+        btnExpandir.setAttribute('src', '../assets/icons/open-icone.png');
+    } else {
+        navOpen = true;
+        btnExpandir.setAttribute('src', '../assets/icons/close-icone.png');
     }
+}
+btnExpandir.addEventListener('click', expandirNav);
 
-    btnExpandir.addEventListener('click', );
+function trocarInputs(btn) {
+    if (btn === btnChangeCpu) {
+        slcCpu.classList.toggle('changed');
+        inputCpu.classList.toggle('changed');
+    } else if (btn === btnChangeRam) {
+        slcRam.classList.toggle('changed');
+        inputRam.classList.toggle('changed');
+    } else if (btn === btnChangeDisc) {
+        slcDisc.classList.toggle('changed');
+        inputDisc.classList.toggle('changed');
+    } else if (btn === btnChangeRede) {
+        slcNet.classList.toggle('changed');
+        inputRede.classList.toggle('changed');
+    }
+}
