dfd7463cae5b090f513b91eb5debbf19c79c3ae5
Luigi Vicchietti <vcchtt.yluigi22@gmail.com>
2023-10-08 02:42:46 -0300

    Criando a segunda tela - data centers

---
+++ site-performee/public/area-restrita/js/dash-geral.js
@@ -0,0 +1,27 @@
+const btnExpandir = document.getElementById('Expand')
+    , nav = document.getElementById('Nav')
+
+    , btnChangeCpu = document.getElementById('ChangeInputCpu')
+    , inputCpu = document.getElementById('InputCpu')
+    , slcCpu = document.getElementById('SlcCpu')
+
+    , btnChangeRam = document.getElementById('ChangeInputRam')
+    , inputRam = document.getElementById('InputRam')
+    , slcRam = document.getElementById('SlcRam')
+
+    , btnChangeDisc = document.getElementById('ChangeInputDisc')
+    , inputDisc = document.getElementById('InputDisc')
+    , slcDisc = document.getElementById('SlcDisc')
+
+    , btnChangeRede = document.getElementById('ChangeInputRede')
+    , inputRede = document.getElementById('InputRede')
+    , slcNet = document.getElementById('SlcNet')
+
+    , inputLog = document.getElementById('InputLog')
+    , slcLog = document.getElementById('SlcLog');
+
+    const expandirNav = () => {
+        nav.classList.toggle('opened');
+    }
+
+    btnExpandir.addEventListener('click', );
